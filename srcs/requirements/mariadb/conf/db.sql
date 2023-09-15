928e8049becba81a59edc11f0161661b01889070
ismail el bakkouchi <iel-bakk@e3r1p3.1337.ma>
2023-09-15 21:13:17 +0100

    still  need to fix mariadb

--- srcs/requirements/mariadb/conf/db.sql
+++ srcs/requirements/mariadb/conf/db.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE if not exists my_database;
-CREATE USER 'UTRIPPEN'@'%';
-GRANT ALL PRIVILEGES ON my_database.* to 'UTRIPPEN'@'%' IDENTIFIED BY '123';
+CREATE DATABASE if not exists inception_db;
+CREATE USER 'iel-bakk'@'%';
+GRANT ALL PRIVILEGES ON inception_db.* to 'iel-bakk'@'%' IDENTIFIED BY 'iel-bakk';
 alter user 'root'@'localhost' identified by '123';
 FLUSH PRIVILEGES;
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,36 +1,49 @@
 #!/bin/sh
-# update the package manaeger and installs any available updates
 apt update -y && apt upgrade -y
 # install three packages "curl, php-form, php-mysql" and download wp-cli.phar
 apt install curl php-fpm php-mysql -y && curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-# make the file executable and move it to /usr/local/bin/wp
+# # make the file executable and move it to /usr/local/bin/wp
+
+mkdir -p /run/php
 chmod +x wp-cli.phar && mv wp-cli.phar /usr/local/bin/wp
-#modifie the PHP-FPM configuration file to change the listen port from the default to 9000
-sed -i -e 's/listen =.*/listen = 9000/g' /etc/php/7.4/fpm/pool.d/www.conf
-# wordpress client is used to download the wordpress files
-wp core download --path="/var/www/html"  --allow-root
-# wordpress client is used to create the wp-config.php file with the database details
-sleep 5
-rm -rf /var/www/html/wp-config.php
 
-echo $MYSQL_DATABASE = $MYSQL_USER = $MYSQL_PASSWORD = $MYSQL_HOST = $URL = $TITLE = $ADMIN_USER = $ADMIN_PASSWORD = $ADMIN_EMAIL = $MYSQL_PORT
-# wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD \
-#     --dbhost=$MYSQL_HOST --path="/var/www/html/" --allow-root --skip-check
-echo "wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST --path=/var/www/html --allow-root --skip-check"
-wp config create --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost="$MYSQL_HOST" --path=/var/www/html --allow-root --skip-check
-sleep 5
-ls /var/www/html/
-sleep 5
-# wordpress client is used to install wordpress with an admin user and password
-wp core install --url=$URL --title=$TITLE \
-    --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD \
-    --admin_email=$ADMIN_EMAIL --allow-root --path=/var/www/html
-# wordpress client is used to create a new user with the role of author
-wp user create iel-bakk iel-bakk@gmail.com --user_pass=$MYSQL_PASSWORD --role=author --allow-root --path=/var/www/html/
+wp core download --path=/var/www/html --allow-root 
+cat /var/www/html/wp-config-sample.php > /var/www/html/wp-content/wp-config.php
+chmod -R 0777 /var/www/html/wp-content
+sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g' /etc/php/7.4/fpm/pool.d/www.conf
 
+chown -R www-data:www-data /var/www/html
 
-# start the php-fpm service and then stop it to creat the pid file
-service php7.4-fpm start
-service php7.4-fpm stop
-# starts the php-fpm service in the foreground so that the container does not exit
+wp config set DB_NAME $MYSQL_DATABASE --path=/var/www/html --allow-root
+wp config set DB_USER $MYSQL_USER --path=/var/www/html --allow-root
+wp config set DB_PASSWORD $MYSQL_ROOT_PASSWORD --path=/var/www/html --allow-root
+wp config set DB_HOST $MYSQL_HOST --path=/var/www/html --allow-root
+
+wp core install --url='iel-bakk.42.fr' --path=/var/www/html --title=$TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root
+wp user create --allow-root  --path=/var/www/html  'iel-bakk' 'iel-bakk-author@student.1337.ma' --role=author --user_pass=$ADMIN_PASSWORD
+# wp config set WP_CACHE true --path=/var/www/ --allow-root
+
 php-fpm7.4 -F
+# #!/bin/sh
+# # update the package manaeger and installs any available updates
+# #modifie the PHP-FPM configuration file to change the listen port from the default to 9000
+# sed -i -e 's/listen =.*/listen = 9000/g' /etc/php/7.4/fpm/pool.d/www.conf
+# # wordpress client is used to download the wordpress files
+# wp core download --path="/var/www/html"  --allow-root
+# mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+# chown -R www-data:www-data /var/www/html
+# # wordpress client is used to create the wp-config.php file with the database details
+# wp config --dbname="saad" --dbuser="ssda2" --dbpass="fdsfdsfs" --dbhost="mariadb" --path="/var/www/html" --allow-root --skip-check
+# # wordpress client is used to install wordpress with an admin user and password
+# wp core install --url=$URL --title=$TITLE \
+#     --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD \
+#     --admin_email=$ADMIN_EMAIL --allow-root --path=/var/www/html
+# # wordpress client is used to create a new user with the role of author
+# wp user create iel-bakk iel-bakk@gmail.com --user_pass=$MYSQL_PASSWORD --role=author --allow-root --path=/var/www/html/
+
+
+# # start the php-fpm service and then stop it to creat the pid file
+# service php7.4-fpm start
+# service php7.4-fpm stop
+# # starts the php-fpm service in the foreground so that the container does not exit
+# php-fpm7.4 -F
